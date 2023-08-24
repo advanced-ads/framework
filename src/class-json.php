5e2d8de7095a3f9c18c92872abbe239698405771
Shakeeb Ahmed <me@shakeebahmed.com>
2023-08-24 11:51:18 +0300

    refactor add function for dynamic arguments

--- src/class-json.php
+++ src/class-json.php
@@ -50,16 +50,15 @@ class JSON {
 	 *
 	 * @throws InvalidArgumentException When object name not defined.
 	 *
-	 * @return JSON
+	 * @return void
 	 */
-	public function hooks() {
+	public function hooks(): void {
 		if ( empty( $this->default_object_name ) ) {
 			throw new InvalidArgumentException( 'Please set default object name to be used when printing JSON.' );
 		}
 
 		$hook = is_admin() ? 'admin_footer' : 'wp_footer';
 		add_action( $hook, [ $this, 'output' ], 0 );
-		return $this;
 	}
 
 	/**
@@ -67,39 +66,62 @@ class JSON {
 	 *
 	 * @since  1.0.0
 	 *
-	 * @param string $key         Unique identifier.
-	 * @param mixed  $value       The data itself can be either a single or an array.
-	 * @param string $object_name Name for the JavaScript object.
-	 *                            Passed directly, so it should be qualified JS variable.
+	 * @param mixed ...$args Arguments.
+	 *
+	 * Parameters can be
+	 * 1. array|string Unique identifier or array<key, value>.
+	 * 2. array|string The data itself can be either a scalar or an array.
+	 *                 In Case of first param an array this can be object_name.
+	 * 3. string Name for the JavaScript object.
+	 *           Passed directly, so it should be qualified JS variable.
+	 *
 	 * @return JSON
 	 */
-	public function add( $key, $value, $object_name = false ) {
+	public function add( ...$args ): JSON {
+		list( $key, $value, $object_name ) = $this->get_add_data( $args );
+
 		// Early Bail!!
 		if ( empty( $key ) ) {
 			return $this;
 		}
 
-		if ( empty( $object_name ) ) {
-			$object_name = $this->default_object_name;
+		// If array is passed.
+		if ( is_array( $key ) ) {
+			foreach ( $key as $arr_key => $arr_value ) {
+				$this->add_to_storage( $arr_key, $arr_value, $object_name );
+			}
+
+			return $this;
 		}
 
+		$this->add_to_storage( $key, $value, $object_name );
+
+		return $this;
+	}
+
+	/**
+	 * Add to storage.
+	 *
+	 * @since  1.0.0
+	 *
+	 * @param string $key         Unique identifier.
+	 * @param mixed  $value       The data itself can be either a single or an array.
+	 * @param string $object_name Name for the JavaScript object.
+	 *                            Passed directly, so it should be qualified JS variable.
+	 * @return void
+	 */
+	private function add_to_storage( $key, $value, $object_name ): void {
 		// If key doesn't exists.
 		if ( ! isset( $this->data[ $object_name ][ $key ] ) ) {
 			$this->data[ $object_name ][ $key ] = $value;
-			return $this;
+			return;
 		}
 
 		// If key already exists.
 		$old_value = $this->data[ $object_name ][ $key ];
+		$is_array  = is_array( $old_value ) && is_array( $value );
 
-		// If both array merge them.
-		if ( is_array( $old_value ) && is_array( $value ) ) {
-			$this->data[ $object_name ][ $key ] = array_merge( $old_value, $value );
-			return $this;
-		}
-
-		$this->data[ $object_name ][ $key ] = $value;
-		return $this;
+		$this->data[ $object_name ][ $key ] = $is_array ? array_merge( $old_value, $value ) : $value;
 	}
 
 	/**
@@ -112,10 +134,10 @@ class JSON {
 	 *                            Passed directly, so it should be qualified JS variable.
 	 * @return JSON
 	 */
-	public function remove( $key, $object_name = false ) {
+	public function remove( $key, $object_name = false ): JSON {
 		// Early Bail!!
 		if ( empty( $key ) ) {
-			return;
+			return $this;
 		}
 
 		if ( empty( $object_name ) ) {
@@ -134,9 +156,9 @@ class JSON {
 	 *
 	 * @since 1.0.0
 	 *
-	 * @return JOSN
+	 * @return JSON
 	 */
-	public function clear_all() {
+	public function clear_all(): JSON {
 		$this->data                               = [];
 		$this->data[ $this->default_object_name ] = [];
 
@@ -147,8 +169,10 @@ class JSON {
 	 * Print data.
 	 *
 	 * @since  1.0.0
+	 *
+	 * @return void
 	 */
-	public function output() {
+	public function output(): void {
 		$script = $this->encode();
 		if ( ! $script ) {
 			return;
@@ -168,7 +192,7 @@ class JSON {
 	 *
 	 * @return string
 	 */
-	private function encode() {
+	private function encode(): string {
 		$script = '';
 		foreach ( $this->data as $object_name => $object_data ) {
 			$script .= $this->single_object( $object_name, $object_data );
@@ -182,11 +206,12 @@ class JSON {
 	 *
 	 * @since  1.0.0
 	 *
-	 * @param  string $object_name Object name to use as JS variable.
-	 * @param  array  $object_data Object data to json encode.
-	 * @return array
+	 * @param string $object_name Object name to use as JS variable.
+	 * @param array  $object_data Object data to json encode.
+	 *
+	 * @return string
 	 */
-	private function single_object( $object_name, $object_data ) {
+	private function single_object( $object_name, $object_data ): string {
 		if ( empty( $object_data ) ) {
 			return '';
 		}
@@ -200,5 +225,29 @@ class JSON {
 		}
 
 		return "var $object_name = " . wp_json_encode( $object_data ) . ';' . PHP_EOL;
+	}
+
+	/**
+	 * Normalize add arguments
+	 *
+	 * @param array $args Arguments array.
+	 *
+	 * @return array
+	 */
+	private function get_add_data( $args ): array {
+		$key         = $args[0] ?? false;
+		$value       = $args[1] ?? false;
+		$object_name = $args[2] ?? $this->default_object_name;
+
+		if ( is_array( $key ) && ! empty( $value ) ) {
+			$object_name = $value;
+			$value       = false;
+		}
+
+		return [
+			$key,
+			$value,
+			$object_name,
+		];
 	}
 }
