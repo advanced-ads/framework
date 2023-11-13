29b47a08491d1ca539228ae65816cdbf1645fa79
Shakeeb Ahmed <me@shakeebahmed.com>
2023-11-13 19:16:06 +0300

    Add new formatting utilities

--- src/utilities/class-arr.php
+++ src/utilities/class-arr.php
@@ -32,29 +32,29 @@ class Arr {
 	/**
 	 * Determine if the given key exists in the provided array.
 	 *
-	 * @param ArrayAccess|array $array Array to check key in.
-	 * @param string|int        $key   Key to check for.
+	 * @param ArrayAccess|array $arr Array to check key in.
+	 * @param string|int        $key Key to check for.
 	 *
 	 * @return bool
 	 */
-	public static function exists( $array, $key ) {
-		if ( $array instanceof ArrayAccess ) {
-			return $array->offsetExists( $key );
+	public static function exists( $arr, $key ) {
+		if ( $arr instanceof ArrayAccess ) {
+			return $arr->offsetExists( $key );
 		}
 
-		return array_key_exists( $key, $array );
+		return array_key_exists( $key, $arr );
 	}
 
 	/**
 	 * Insert a single array item inside another array at a set position
 	 *
-	 * @param array $array    Array to modify. Is passed by reference, and no return is needed.
+	 * @param array $arr      Array to modify. Is passed by reference, and no return is needed.
 	 * @param array $new      New array to insert.
 	 * @param int   $position Position in the main array to insert the new array.
 	 */
-	public static function insert( &$array, $new, $position ) {
-		$before = array_slice( $array, 0, $position - 1 );
-		$after  = array_diff_key( $array, $before );
-		$array  = array_merge( $before, $new, $after );
+	public static function insert( &$arr, $new, $position ) {
+		$before = array_slice( $arr, 0, $position - 1 );
+		$after  = array_diff_key( $arr, $before );
+		$arr    = array_merge( $before, $new, $after );
 	}
 }
