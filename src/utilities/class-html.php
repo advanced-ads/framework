36ef24b1cae8c56799e8990cbb810379fc9f3e2f
Shakeeb Ahmed <me@shakeebahmed.com>
2024-06-04 01:10:55 +0300

    Add new utilities

--- src/utilities/class-html.php
+++ src/utilities/class-html.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * String utilities
+ * HTML formatting utilities
  *
  * @package AdvancedAds\Framework\Utilities
  * @author  Advanced Ads <info@wpadvancedads.com>
@@ -12,65 +12,24 @@ namespace AdvancedAds\Framework\Utilities;
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Str class.
+ * HTML class.
  */
-class Str {
-	/**
-	 * Validates whether the passed variable is a empty string.
-	 *
-	 * @param mixed $variable The variable to validate.
-	 *
-	 * @return bool Whether or not the passed value is a non-empty string.
-	 */
-	public static function is_empty( $variable ): bool {
-		return ! is_string( $variable ) || empty( $variable );
-	}
+class HTML {
 
 	/**
-	 * Validates whether the passed variable is a non-empty string.
+	 * Implode and escape HTML attributes for output.
 	 *
-	 * @param mixed $variable The variable to validate.
+	 * @param array $raw_attributes Attribute name value pairs.
 	 *
-	 * @return bool Whether or not the passed value is a non-empty string.
+	 * @return string
 	 */
-	public static function is_non_empty( $variable ): bool {
-		return is_string( $variable ) && '' !== $variable;
-	}
+	public static function build_attributes( $raw_attributes ) {
+		$attributes = [];
+		foreach ( $raw_attributes as $name => $value ) {
+			$attributes[] = esc_attr( $name ) . '="' . esc_attr( $value ) . '"';
+		}
 
-	/**
-	 * Check if the string contains the given value.
-	 *
-	 * @param string $needle   The sub-string to search for.
-	 * @param string $haystack The string to search.
-	 *
-	 * @return bool
-	 */
-	public static function contains( $needle, $haystack ): bool {
-		return self::is_non_empty( $needle ) ? strpos( $haystack, $needle ) !== false : false;
-	}
-
-	/**
-	 * Check if the string begins with the given value.
-	 *
-	 * @param string $needle   The sub-string to search for.
-	 * @param string $haystack The string to search.
-	 *
-	 * @return bool
-	 */
-	public static function starts_with( $needle, $haystack ): bool {
-		return '' === $needle || substr( $haystack, 0, strlen( $needle ) ) === (string) $needle;
-	}
-
-	/**
-	 * Check if the string end with the given value.
-	 *
-	 * @param string $needle   The sub-string to search for.
-	 * @param string $haystack The string to search.
-	 *
-	 * @return bool
-	 */
-	public static function ends_with( $needle, $haystack ): bool {
-		return '' === $needle || substr( $haystack, -strlen( $needle ) ) === (string) $needle;
+		return implode( ' ', $attributes );
 	}
 
 	/**
