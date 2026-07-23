b8697b5b67cc31e774c31833508a32554750cf34
Shakeeb Ahmed <me@shakeebahmed.com>
2026-07-23 15:26:27 +0300

    Update class-params.php

--- src/utilities/class-params.php
+++ src/utilities/class-params.php
@@ -23,21 +23,6 @@ namespace AdvancedAds\Framework\Utilities;
 class Params {
 
 	/**
-	 * Get field from input.
-	 *
-	 * @param string $input   Input to get from.
-	 * @param string $id      Field id to get.
-	 * @param mixed  $default Default value to return if field is not found.
-	 * @param int    $filter  The ID of the filter to apply.
-	 * @param int    $flag    The ID of the flag to apply.
-	 *
-	 * @return mixed
-	 */
-	private static function input( $input, $id, $default = false, $filter = FILTER_DEFAULT, $flag = [] ) {
-		return filter_has_var( $input, $id ) ? filter_input( $input, $id, $filter, $flag ) : $default;
-	}
-
-	/**
 	 * Get field from query string.
 	 *
 	 * @param string $id      Field id to get.
@@ -66,7 +51,9 @@ class Params {
 	}
 
 	/**
-	 * Get field from FORM server.
+	 * Get field from request.
+	 *
+	 * Honors PHP `request_order` (default GP) so later sources override earlier ones.
 	 *
 	 * @param string $id      Field id to get.
 	 * @param mixed  $default Default value to return if field is not found.
@@ -75,8 +62,29 @@ class Params {
 	 *
 	 * @return mixed
 	 */
-	public static function server( $id, $default = false, $filter = FILTER_DEFAULT, $flag = [] ) {
-		return isset( $_SERVER[ $id ] ) ? filter_var( wp_unslash( $_SERVER[ $id ] ), $filter, $flag ) : $default;
+	public static function request( $id, $default = false, $filter = FILTER_DEFAULT, $flag = [] ) {
+		$request_filters = [
+			'G' => INPUT_GET,
+			'P' => INPUT_POST,
+			'C' => INPUT_COOKIE,
+		];
+
+		$request_order = ini_get( 'request_order' ) ? ini_get( 'request_order' ) : 'GP';
+		$request_order = array_reverse( str_split( $request_order ) );
+		$not_found     = new \stdClass();
+
+		foreach ( $request_order as $r ) {
+			if ( ! isset( $request_filters[ $r ] ) ) {
+				continue;
+			}
+
+			$value = self::input( $request_filters[ $r ], $id, $not_found, $filter, $flag );
+			if ( $not_found !== $value ) {
+				return $value;
+			}
+		}
+
+		return $default;
 	}
 
 	/**
@@ -94,6 +102,20 @@ class Params {
 	}
 
 	/**
+	 * Get field from FORM server.
+	 *
+	 * @param string $id      Field id to get.
+	 * @param mixed  $default Default value to return if field is not found.
+	 * @param int    $filter  The ID of the filter to apply.
+	 * @param int    $flag    The ID of the flag to apply.
+	 *
+	 * @return mixed
+	 */
+	public static function server( $id, $default = false, $filter = FILTER_DEFAULT, $flag = [] ) {
+		return isset( $_SERVER[ $id ] ) ? filter_var( wp_unslash( $_SERVER[ $id ] ), $filter, $flag ) : $default;
+	}
+
+	/**
 	 * Get field from FORM env.
 	 *
 	 * @param string $id      Field id to get.
@@ -108,8 +130,12 @@ class Params {
 	}
 
 	/**
-	 * Get field from request.
+	 * Get field from input.
 	 *
+	 * Falls back to the matching PHPUnit-friendly superglobal when filter_input
+	 * cannot see the value (CLI / tests that assign $_GET/$_POST directly).
+	 *
+	 * @param int    $input   Input to get from.
 	 * @param string $id      Field id to get.
 	 * @param mixed  $default Default value to return if field is not found.
 	 * @param int    $filter  The ID of the filter to apply.
@@ -117,25 +143,37 @@ class Params {
 	 *
 	 * @return mixed
 	 */
-	public static function request( $id, $default = false, $filter = FILTER_DEFAULT, $flag = [] ) {
-		$request_filters = [
-			'G' => INPUT_GET,
-			'P' => INPUT_POST,
-			'C' => INPUT_COOKIE,
-		];
-
-		// This directive describes the order in which PHP registers GET, POST and Cookie variables into the _REQUEST array. Registration is done from left to right, newer values override older values.
-		$request_order = ini_get( 'request_order' ) ? ini_get( 'request_order' ) : 'GP';
-		$request_order = str_split( $request_order );
-		$request_order = array_reverse( $request_order );
-
-		foreach ( $request_order as $r ) {
-			if ( filter_has_var( $request_filters[ $r ], $id ) ) {
-				// Return early if found.
-				return filter_input( $request_filters[ $r ], $id, $filter, $flag );
-			}
+	private static function input( $input, $id, $default = false, $filter = FILTER_DEFAULT, $flag = [] ) {
+		if ( filter_has_var( $input, $id ) ) {
+			return filter_input( $input, $id, $filter, $flag );
 		}
 
-		return $default;
+		$value = self::superglobal_value( $input, $id );
+		if ( null === $value ) {
+			return $default;
+		}
+
+		return filter_var( wp_unslash( $value ), $filter, $flag );
+	}
+
+	/**
+	 * Read a raw value from a request/cookie bag when filter_input is unavailable.
+	 *
+	 * @param int    $input Input constant.
+	 * @param string $id    Field id.
+	 *
+	 * @return mixed|null
+	 */
+	private static function superglobal_value( $input, $id ) {
+		switch ( $input ) {
+			case INPUT_GET:
+				return array_key_exists( $id, $_GET ) ? $_GET[ $id ] : null; // phpcs:ignore WordPress.Security.NonceVerification.Recommended
+			case INPUT_POST:
+				return array_key_exists( $id, $_POST ) ? $_POST[ $id ] : null; // phpcs:ignore WordPress.Security.NonceVerification.Missing
+			case INPUT_COOKIE:
+				return array_key_exists( $id, $_COOKIE ) ? $_COOKIE[ $id ] : null;
+			default:
+				return null;
+		}
 	}
 }
